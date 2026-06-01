b0bb481640e0e044eb0eb78fa0e1cb34a9856491
meymeyw <meya.wikner@gmail.com>
2026-06-01 22:31:48 +0200

    Updated gd scripts

--- Scripts/CursorController.gd
+++ Scripts/CursorController.gd
@@ -3,20 +3,19 @@ extends Node2D
 @export var dualGridTilemap : DualGridTilemap
 
 func _process(_delta):
-	var update = false
-	var coords: Vector2i = dualGridTilemap.local_to_map(global_position)
+	var coords: Vector2i = dualGridTilemap.localToMap(global_position)
 
 	if Input.is_action_pressed("left_click"):
-		dualGridTilemap.set_cell(0, coords, 0, dualGridTilemap.dirtPlaceholderAtlasCoord)
-		update = true
-
+		dualGridTilemap.setTile(coords, dualGridTilemap.TerrainType.DIRT)
+	elif Input.is_action_pressed("middle_click"):
+		dualGridTilemap.setTile(coords, DualGridTilemap.TerrainType.GRASS)
 	elif Input.is_action_pressed("right_click"):
-		dualGridTilemap.set_cell(0, coords, 0, dualGridTilemap.grassPlaceholderAtlasCoord)
-		update = true
-
-	if update == true:
-		dualGridTilemap._reloadDisplayTiles()
-
+		dualGridTilemap.setTile(coords, DualGridTilemap.TerrainType.SAND)
 
 func _physics_process(_delta):
-	global_position = get_global_mouse_position()
+	global_position = _get_world_pos_tile(get_global_mouse_position()) + Vector2(8, 8)
+
+static func _get_world_pos_tile(world_pos: Vector2) -> Vector2:
+	var x := floori(world_pos.x / DualGridTilemap.TILE_SIZE) * DualGridTilemap.TILE_SIZE
+	var y := floori(world_pos.y / DualGridTilemap.TILE_SIZE) * DualGridTilemap.TILE_SIZE
+	return Vector2(x, y)
--- Scripts/DualGridTilemap.gd
+++ Scripts/DualGridTilemap.gd
@@ -2,74 +2,121 @@ extends Node2D
 
 class_name DualGridTilemap
 
-@onready var worldLayer : TileMapLayer = $WorldLayer
-@export var displayTilemap : TileMapLayer # point this at one of your display layers
-@export var grassPlaceholderAtlasCoord : Vector2i
-@export var dirtPlaceholderAtlasCoord : Vector2i
+const TILE_SIZE = 16
+const HALF_TILE_SIZE = TILE_SIZE / 2
 
-var NEIGHBOURS : Array = [
+var NEIGHBOURS : Array[Vector2i] = [
 	Vector2i(0, 0),
 	Vector2i(1, 0),
 	Vector2i(0, 1),
 	Vector2i(1, 1)
 ]
 
-var neighboursToAtlasCoord : Dictionary = {
-	"Grass, Grass, Grass, Grass" = Vector2i(2, 1), 		# All corners
-	"Dirt, Dirt, Dirt, Grass" = Vector2i(1, 3), 		# Outer bottom-right corner
-	"Dirt, Dirt, Grass, Dirt" = Vector2i(0, 0), 		# Outer bottom-left corner
-	"Dirt, Grass, Dirt, Dirt" = Vector2i(0, 2), 		# Outer top-right corner
-	"Grass, Dirt, Dirt, Dirt" = Vector2i(3, 3), 		# Outer top-left corner
-	"Dirt, Grass, Dirt, Grass" = Vector2i(1, 0), 		# Right edge
-	"Grass, Dirt, Grass, Dirt" = Vector2i(3, 2), 		# Left edge
-	"Dirt, Dirt, Grass, Grass" = Vector2i(3, 0), 		# Bottom edge
-	"Grass, Grass, Dirt, Dirt" = Vector2i(1, 2), 		# Top edge
-	"Dirt, Grass, Grass, Grass" = Vector2i(1, 1), 		# Inner bottom-right corner
-	"Grass, Dirt, Grass, Grass" = Vector2i(2, 0), 		# Inner bottom-left corner
-	"Grass, Grass, Dirt, Grass" = Vector2i(2, 2), 		# Inner top-right corner
-	"Grass, Grass, Grass, Dirt" = Vector2i(3, 1), 		# Inner top-left corner
-	"Dirt, Grass, Grass, Dirt" = Vector2i(2, 3), 		# Bottom-left top-right corners
-	"Grass, Dirt, Dirt, Grass" = Vector2i(0, 1), 		# Top-left down-right corners
-	"Dirt, Dirt, Dirt, Dirt" = Vector2i(0, 3) 			# No corners
+# Key: [topLeft, topRight, botLeft, botRight] as TileType ints
+# Value: atlas coord on the display tileset
+const neighboursToAtlasCoord : Dictionary = {
+	[TileType.SOME, TileType.SOME, TileType.SOME, TileType.SOME]: Vector2i(2, 1),  # All corners
+	[TileType.NONE, TileType.NONE, TileType.NONE, TileType.SOME]: Vector2i(1, 3),  # Outer bottom-right corner
+	[TileType.NONE, TileType.NONE, TileType.SOME, TileType.NONE]: Vector2i(0, 0),  # Outer bottom-left corner
+	[TileType.NONE, TileType.SOME, TileType.NONE, TileType.NONE]: Vector2i(0, 2),  # Outer top-right corner
+	[TileType.SOME, TileType.NONE, TileType.NONE, TileType.NONE]: Vector2i(3, 3),  # Outer top-left corner
+	[TileType.NONE, TileType.SOME, TileType.NONE, TileType.SOME]: Vector2i(1, 0),  # Right edge
+	[TileType.SOME, TileType.NONE, TileType.SOME, TileType.NONE]: Vector2i(3, 2),  # Left edge
+	[TileType.NONE, TileType.NONE, TileType.SOME, TileType.SOME]: Vector2i(3, 0),  # Bottom edge
+	[TileType.SOME, TileType.SOME, TileType.NONE, TileType.NONE]: Vector2i(1, 2),  # Top edge
+	[TileType.NONE, TileType.SOME, TileType.SOME, TileType.SOME]: Vector2i(1, 1),  # Inner bottom-right corner
+	[TileType.SOME, TileType.NONE, TileType.SOME, TileType.SOME]: Vector2i(2, 0),  # Inner bottom-left corner
+	[TileType.SOME, TileType.SOME, TileType.NONE, TileType.SOME]: Vector2i(2, 2),  # Inner top-right corner
+	[TileType.SOME, TileType.SOME, TileType.SOME, TileType.NONE]: Vector2i(3, 1),  # Inner top-left corner
+	[TileType.NONE, TileType.SOME, TileType.SOME, TileType.NONE]: Vector2i(2, 3),  # Bottom-left top-right corners
+	[TileType.SOME, TileType.NONE, TileType.NONE, TileType.SOME]: Vector2i(0, 1),  # Top-left bottom-right corners
+	[TileType.NONE, TileType.NONE, TileType.NONE, TileType.NONE]: Vector2i(-1, -1) # No corners — treated as empty
 }
 
-func _ready():
-	_reloadDisplayTiles()
+enum TileType { NONE, SOME }
+enum TerrainType {
+	EMPTY = -1,
+	GRASS = 1,
+	DIRT = 2,
+	SAND = 3
+}
 
-func _reloadDisplayTiles():
-	# Refresh all display tiles
-	for coord : Vector2i in worldLayer.get_used_cells():
-		setDisplayTile(coord)
+@export var worldMapLayer : TileMapLayer
+@export var dirtDisplayMapLayer : TileMapLayer
+@export var sandDisplayMapLayer : TileMapLayer
+@export var grassDisplayMapLayer : TileMapLayer
+@export var dirtPlaceholderAtlasCoords : Vector2i
+@export var sandPlaceholderAtlasCoords : Vector2i
+@export var grassPlaceholderAtlasCoords : Vector2i
 
-func SetTile(coords: Vector2i, atlasCoords: Vector2i):
-	worldLayer.set_cell(coords, 0, atlasCoords)
-	setDisplayTile(coords)
+var placeholderSourceId : int = 0
 
-func setDisplayTile(pos: Vector2i):
-	var i: int = 0
-	i += 1
-	var newPos: Vector2i = pos + NEIGHBOURS[i]
-	displayTilemap.set_cell(newPos, 0, calculateDisplayTile(newPos), 0)
+func _ready() -> void:
+	_refreshAllTiles()
 
-func calculateDisplayTile(coords: Vector2i):
-	# get 4 world tile neighbours
-	var botRight = getWorldTile(coords - NEIGHBOURS[0])
-	var botLeft = getWorldTile(coords - NEIGHBOURS[1])
-	var topRight = getWorldTile(coords - NEIGHBOURS[2])
-	var topLeft = getWorldTile(coords - NEIGHBOURS[3])
+func _refreshAllTiles() -> void:
+	for cell_pos : Vector2i in worldMapLayer.get_used_cells():
+		_refreshDisplayTile(cell_pos, grassDisplayMapLayer, TerrainType.GRASS)
+		_refreshDisplayTile(cell_pos, dirtDisplayMapLayer, TerrainType.DIRT)
+		_refreshDisplayTile(cell_pos, sandDisplayMapLayer, TerrainType.SAND)
 
-	# return tile (atlas coord) that fits the neighbour rules
-	return neighboursToAtlasCoord[str(topLeft, ", ", topRight, ", ", botLeft, ", ", botRight)]
+func localToMap(pos: Vector2) -> Vector2i:
+	return worldMapLayer.local_to_map(pos)
 
-func getWorldTile(coords: Vector2i):
-	var atlasCoord = worldLayer.get_cell_atlas_coords(coords)
-	if atlasCoord == grassPlaceholderAtlasCoord:
-		return "Grass"
+func setTile(coords: Vector2i, terrain_type: TerrainType) -> void:
+	var oldTerrainType : TerrainType = getTerrainType(coords)
+
+	worldMapLayer.set_cell(coords, placeholderSourceId, getPlaceholderAtlasCoords(terrain_type))
+	_refreshDisplayTile(coords, getDisplayLayer(terrain_type), terrain_type)
+	
+	# Refresh old display layer if the terrain type has changed
+	if oldTerrainType != terrain_type && oldTerrainType != TerrainType.EMPTY:
+		_refreshDisplayTile(coords, getDisplayLayer(oldTerrainType), oldTerrainType)
+
+func _refreshDisplayTile(pos: Vector2i, display_layer: TileMapLayer, terrain_type: TerrainType) -> void:
+	for i in range(NEIGHBOURS.size()):
+		var newPos : Vector2i = pos + NEIGHBOURS[i]
+		var atlasCoords : Vector2i = _calculateDisplayTileAtlasCoords(newPos, terrain_type)
+		if atlasCoords == -Vector2i.ONE:
+			display_layer.erase_cell(newPos)
+		else:
+			display_layer.set_cell(newPos, terrain_type, atlasCoords)
+
+func _calculateDisplayTileAtlasCoords(coords: Vector2i, terrain_type: TerrainType) -> Vector2i:
+	var bot_right : TileType = _getMatchingTileType(coords - NEIGHBOURS[0], terrain_type)
+	var bot_left  : TileType = _getMatchingTileType(coords - NEIGHBOURS[1], terrain_type)
+	var top_right : TileType = _getMatchingTileType(coords - NEIGHBOURS[2], terrain_type)
+	var top_left  : TileType = _getMatchingTileType(coords - NEIGHBOURS[3], terrain_type)
+
+	return neighboursToAtlasCoord[[top_left, top_right, bot_left, bot_right]]
+
+func _getMatchingTileType(coords: Vector2i, terrain_type: TerrainType) -> TileType:
+	var targetCoords: Vector2i = getPlaceholderAtlasCoords(terrain_type)
+	var atlasCoord: Vector2i = worldMapLayer.get_cell_atlas_coords(coords)
+	if atlasCoord != targetCoords:
+		return TileType.NONE
+	return TileType.SOME
+
+func getPlaceholderAtlasCoords(terrain_type: TerrainType) -> Vector2i:
+	match terrain_type:
+		TerrainType.GRASS	: return grassPlaceholderAtlasCoords
+		TerrainType.DIRT	: return  dirtPlaceholderAtlasCoords
+		TerrainType.SAND	: return  sandPlaceholderAtlasCoords
+		_					: return -Vector2i.ONE
+
+func getDisplayLayer(terrain_type: TerrainType) -> TileMapLayer:
+	match terrain_type:
+		TerrainType.GRASS : return grassDisplayMapLayer
+		TerrainType.DIRT  : return  dirtDisplayMapLayer
+		TerrainType.SAND  : return  sandDisplayMapLayer
+		_: return null
+
+func getTerrainType(cell_pos: Vector2i) -> TerrainType:
+	var coords : Vector2i = worldMapLayer.get_cell_atlas_coords(cell_pos)
+	if   coords == grassPlaceholderAtlasCoords	: return TerrainType.GRASS
+	elif coords ==  dirtPlaceholderAtlasCoords	: return TerrainType.DIRT
+	elif coords ==  sandPlaceholderAtlasCoords	: return TerrainType.SAND
+	elif coords == -Vector2i.ONE				: return TerrainType.EMPTY
 	else:
-		return "Dirt"
-
-enum TileType {
-	None,
-	Grass,
-	Dirt
-}
+		push_error("Unknown placeholder atlas coords: %s. Defaulting to Grass." % coords)
+		return TerrainType.GRASS
